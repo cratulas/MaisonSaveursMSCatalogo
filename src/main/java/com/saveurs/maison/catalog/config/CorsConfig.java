7dd2e91d86880a764be4c2072da62809b4e7f576
Mauricio Mora <mauricios.mora@gmail.com>
2025-11-24 00:17:31 +1300

    Cors

---
+++ src/main/java/com/saveurs/maison/catalog/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.saveurs.maison.catalog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "http://localhost:8100",   // Ionic web
+                                "capacitor://localhost",   // App nativa Android/iOS
+                                "ionic://localhost"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
